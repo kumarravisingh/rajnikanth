e403ea8b0cb3d948af364c4998456761a873d4de
kumar ravi <hi@kumarravi.me>
2019-02-13 19:53:38 +0530

    bug fix in name
    Signed-off-by: kumar ravi <hi@kumarravi.me>

--- src/RajnikanthServiceProvider.php
+++ src/RajnikanthServiceProvider.php
@@ -4,7 +4,7 @@ namespace KumarRavi\Rajnikanth;
 
 use Carbon\Laravel\ServiceProvider;
 
-class RajnikanthServiceProvide extends ServiceProvider
+class RajnikanthServiceProvider extends ServiceProvider
 {
     public function boot(){
 
